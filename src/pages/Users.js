c2560c60a2bf340daf75e40467d91017ec6fd3da
albertofragoso <hola@albertofragoso.com>
2019-06-26 23:36:29 -0500

    Changing Users page from useful to useless component.

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import './styles/Users.css'
 
-class User extends Component {
-  render() {
-    return(
-      <div className="Users">
-        <div className="Users__hero">
-          <h1>
-            <span className="font-weight-light">Blog</span> 
-            <span className="font-weight-bold">Redux</span>
-          </h1>
-        </div>
-        <div className="Users__container">
-          Something...
-        </div>
+const Users = () => {
+  return(
+    <div className="Users">
+      <div className="Users__hero">
+        <h1>
+          <span className="font-weight-light">Blog</span> 
+          <span className="font-weight-bold">Redux</span>
+        </h1>
       </div>
-    )
-  }
+      <div className="Users__container">
+        Something...
+      </div>
+    </div>
+  )
 }
 
-export default User
+export default Users
