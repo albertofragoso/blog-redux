44d311da876d98414e7a3c308cbcaae1ab741863
albertofragoso <hola@albertofragoso.com>
2019-06-26 22:24:56 -0500

    Creating usersReducer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './styles/Footer.css'
 
 const Footer = () => (
   <div className="Footer">
-    <span>Made with 💛</span>
+    <span className="font-weight-light">Made with 💛 by</span> <span className="font-weight-bold">Alberto Fragoso</span>
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 
+const store = createStore(
+  reducers, // These are all reducers
+  {}  // Main state
+)
 
-import App from './App';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
