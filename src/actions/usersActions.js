3d51a49e8567f68f484652211510e732701d95bf
albertofragoso <hola@albertofragoso.com>
2019-06-27 23:37:37 -0500

    Try-catch promise usersActions.

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,9 +2,14 @@ import axios from 'axios'
 import { GET_ALL } from '../types/usersTypes'
 
 export const getAll = () => async dispatch => {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-  dispatch({
-    type: GET_ALL,
-    payload: response.data
-  })
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    dispatch({
+      type: GET_ALL,
+      payload: response.data
+    })
+  } catch(error) {
+    console.log('Error: ', error.message)
+  }
+  
 }
