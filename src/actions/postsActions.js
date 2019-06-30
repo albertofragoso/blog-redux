f6013b190281a674052cd6c3d9c97d0eb3efce9a
albertofragoso <hola@albertofragoso.com>
2019-06-29 19:32:49 -0500

    Creating postsActions.

---
+++ src/actions/postsActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { GET_ALL, LOADING, ERROR } from '../types/postsTypes'
+
+export const getAll = () => async dispatch => {
+  
+  dispatch({
+    type: LOADING
+  })
+
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    dispatch({
+      type: GET_ALL,
+      payload: response.data
+    })
+  } catch(error) {
+    dispatch({
+      type: ERROR,
+      payload: error.message
+    })
+  }
+
+}
